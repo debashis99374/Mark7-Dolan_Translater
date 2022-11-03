685f5b5e758cd81606dca7ebb1b21f453ad2c6c0
debashis99374 <debashis99374@gmail.com>
2022-11-03 16:22:22 +0530

    section- wise- devide

--- html.js
+++ html.js
@@ -1,19 +1,20 @@
-var buttonHandler=document.getElementById("button");
-var txtInput=document.getElementById("txt-area-input");
-var outputText=document.getElementById("txt-area-output");
-var serverUrl="https://api.funtranslations.com/translate/dolan.json"
+//importing all html elements required //
+var buttonHandler = document.getElementById("button");
+var txtInput = document.getElementById("txt-area-input");
+var outputText = document.getElementById("txt-area-output");
+var serverUrl = "https://api.funtranslations.com/translate/dolan.json"
+//importing all html elements required //
 
-
-function getTranslationUrl(text){
-    return serverUrl + "?"+"text="+ text
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text
 }
-
-function errorHandler(error){
-    console.log("error occured",error);
+//get a function ready for preparing translation url//
+function errorHandler(error) {
+    console.log("error occured", error);
     alert("issue with server.Please try after some time!!");
 
 }
-
+//getting a function for error handling
 function clickHandler() {
     var inputText = txtInput.value;
     fetch(getTranslationUrl(inputText))
@@ -24,4 +25,6 @@ function clickHandler() {
         })
         .catch(errorHandler)
 }
+//getting a function ready to take translation url, input data, fetch it &then return the output in translated language//
 buttonHandler.addEventListener("click", clickHandler);
+//putting all things together//
